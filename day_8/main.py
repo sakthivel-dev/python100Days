1dc6b164dbc2dd75ae20f49e47059984e65af3a4
sakthivel_mageshwaran <sakthivel.mageshwaran@finstein.ai>
2023-06-23 23:44:26 +0530

    day8

---
+++ day_8/main.py
@@ -0,0 +1,11 @@
+# import the file
+import mymodules
+
+#  call the function
+mymodules.printName()
+mymodules.printNumber()
+
+# passing value on the class 
+tiger = mymodules.Animal('tiger')
+
+tiger.printAnimalName()
